1cf6f85be750d0684fcf2798146ce58e4381ff30
nairmai <mai.nair34@gmail.com>
2018-03-13 13:48:42 -0700

    Recursive minimax with depth 5

--- player.cpp
+++ player.cpp
@@ -1,7 +1,6 @@
 #include "player.hpp"
 #include <climits>
 #include <vector>
-using namespace std;
 //made a change
 // testing - mn
 
@@ -10,7 +9,7 @@ using namespace std;
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
  */
- 
+ int MINIMAX_DEPTH = 5;
  Board *temp_board;
 Player::Player(Side side) 
 {
@@ -42,166 +41,210 @@ Player::~Player()
  * The move returned must be legal; if there are no valid moves for your side,
  * return nullptr.
  */
-
-Move *Player::minimax_iterative(){
-	
-	// Assign sides to each player
-     Side oppSide = WHITE;
-     if(player_side == WHITE)
-     {
+ 
+Move *Player::minimax(Move *currentMove, int msLeft, int depth){
+	//Assign sides to each player
+    Side oppSide = WHITE;
+    if(player_side == WHITE)
+    {
 		 oppSide = BLACK; 
-	 }
-	
-	int max_score = INT_MIN;
-	Move *max_score_move = nullptr;
-	
-	vector<Move *> possible_moves;
-	for(int i = 0; i < 8; i++)
+	}
+	 
+	// base case: if depth is MINIMAX_DEPTH - return move with lowest score
+	if(depth == MINIMAX_DEPTH)
+	{
+		int min_score = INT_MAX;
+		Move *min_score_move = nullptr;
+		vector<Move *> possible_moves;
+		Move *temp_move;
+		
+		for(int i = 0; i < 8; i++)
 		{
-		 for(int j = 0; j < 8; j++)
+			for(int j = 0; j < 8; j++)
 			{
-				Move *move = new Move(i, j);
+				 temp_move = new Move(i, j);
 			 
 				// If the move is valid
-				if(board.checkMove(move, player_side))
-					{
-						possible_moves.push_back(move);	 
-				 
-					}
+				if(board.checkMove(temp_move, player_side))
+				{
+					possible_moves.push_back(temp_move);	 
+				}
 			}		
 		}
 		
-		if(possible_moves.size() == 1){
-			board.doMove(possible_moves[0], player_side);
-			return possible_moves[0];
-			
-		}
-		cerr << "Num possible move: " << possible_moves.size() << endl;
-
-	
-	for(unsigned int i = 0; i < possible_moves.size(); i++){
-		Board *temp_board = board.copy();
-		 temp_board->doMove(possible_moves[i], player_side);
-		 int depth_1_score = temp_board->count(player_side) - temp_board->count(oppSide);
-		 
-		 vector<Move *> possible_opp_moves;
-		 for(int i = 0; i < 8; i++)
+		for(unsigned int i = 0; i < possible_moves.size(); i++)
 		{
-		 for(int j = 0; j < 8; j++)
-			{
-				Move *opp_move = new Move(i, j);
-			 
-				// If the move is valid
-				if(temp_board->checkMove(opp_move, oppSide))
-					{
-						possible_opp_moves.push_back(opp_move);	 
+			Board *temp_board = board.copy();
+			temp_board->doMove(possible_moves[i], player_side);
 				 
-					}
-			}		
-		}
-				cerr << "Num possible opp move: " << possible_opp_moves.size() << endl;
-
-		for(unsigned int j = 0; j < possible_opp_moves.size(); j++){
-			Board *temp2 = temp_board->copy();
-			temp2->doMove(possible_opp_moves[j], oppSide);
-			int depth_2_score = temp2->count(player_side) - temp2->count(oppSide);
-			if(depth_1_score + depth_2_score > max_score){
-				max_score = depth_1_score + depth_2_score;
-				max_score_move = possible_moves[i];
+			// Assign a base score based on the difference between 
+			// number of pieces on the board after the move has been made
+			int score = temp_board->count(player_side) - temp_board->count(oppSide);
+			
+			int move_x = possible_moves[i]->x;
+			int move_y = possible_moves[i]->y;
+			
+			// Apply multipliers based on the cell location on the board
+			// and how favorable a move at that location would be.
+				 
+			// Corner squares are most preferable since these can never be captured
+			if((move_x == 0 && move_y ==0) || 
+			(move_x == 7 && move_y == 0) || 
+			(move_x == 0 && move_y == 7) || 
+			(move_x == 7 && move_y == 7))
+			{
+				score *= 5; 
+			}
+			// Squares adjacent to corners are worse - negative multiplier
+			// quadrant 1
+			else if((move_x == 0 && move_y == 1 )|| 
+			(move_x == 1 && (move_y == 0 || move_y == 1)))
+			{
+				score *= -3;
+			} 
+			// quadrant 2
+			else if((move_x == 6 && (move_y == 0 || move_y == 1)) || 
+			(move_x == 7 && move_y == 1))
+			{
+				score *= -3;
+			}
+			// quadrant 3
+			else if((move_x == 0 && move_y == 6 ) || 
+			(move_x == 1 && (move_y == 6 || move_y == 7)))
+			{
+				score *= -3;
+			}
+			// quadrant 4
+			else if((move_x == 6 && (move_y == 6 || move_y == 7) ) ||
+			 (move_x == 7 && move_y == 6))
+			{
+				score *= -3;
+			}
+				 
+			// Squares at the edge of the board can only be captured
+			// by other edge pieces - slightly favorable
+			else if((move_x == 0) || (move_x == 7) || (move_y == 0) || (move_y == 7))
+			{
+				score *= 3;
 			}
 			
-			delete temp2;
-			
-			
-		}
-		
-		delete temp_board;
-	}
-	
-	//cerr << "move: " << max_score_move->x << ", " << max_score_move->y << endl;
-	
-	board.doMove(max_score_move, player_side);
-	return max_score_move;
-	
-}
-
-/*
-Move *minimax(Move *currentMove, int msLeft, int depth){
-	// Assign sides to each player
-     Side oppSide = WHITE;
-     if(player_side == WHITE)
-     {
-		 oppSide = BLACK; 
-	 }
-	 
-	//base case: if depth is MINIMAX_DEPTH - return move with lowest score
-	if(depth == MINIMAX_DEPTH){
-		int max_score = INT_MIN;
-		Move *max_score_move = nullptr;
-		vector<Move *> possible_moves;
-		 for(int i = 0; i < 8; i++)
-		{
-		 for(int j = 0; j < 8; j++)
+			if(score < min_score)
 			{
-				move = new Move(i, j);
-			 
-				// If the move is valid
-				if(board.checkMove(move, player_side))
-					{
-						possible_moves.push_back(move);	 
-				 
-					}
-			}		
-		}
-		
-		for(int i = 0; i < possible_moves.size(); i++){
-			//Board *temp_board = board.copy();
-			temp_board->doMove(possible_moves[i], oppSide);
-				 
-				 // Assign a base score based on the difference between 
-				 // number of pieces on the board after the move has been made
-			int score = temp_board->count(player_side) - temp_board->count(oppSide);
-			if(score > min_score){
-				max_score = score;
-				max_score_move = move;
+				min_score = score;
+				min_score_move = possible_moves[i];
 			}
 		}
 		
 		return min_score_move;
 	}
 
+	 else // not base case
 	 
-     
-     // Initialize variables
-	
-	 vector<Move *> possible_moves;
-	 // Iterate through all board cells to get all possible player moves
-	 for(int i = 0; i < 8; i++)
 	 {
-		 for(int j = 0; j < 8; j++)
-		 {
-			 player_move = new Move(i, j);
+		// Initialize variables
+		vector<Move *> possible_moves;
+		
+		// Iterate through all board cells to get all possible player moves
+		for(int i = 0; i < 8; i++)
+		{
+			for(int j = 0; j < 8; j++)
+			{
+				Move *player_move = new Move(i, j);
 			 
-			 // If the move is valid
-			 if(board.checkMove(player_move, player_side))
-			 {
-				 possible_moves.push_back(player_move);	 
+				// If the move is valid
+				if(board.checkMove(player_move, player_side))
+				{
+					possible_moves.push_back(player_move);	 
 				 
 				}
-	}	
+			}	
+		
+		}
+	
+		int max_score = INT_MIN;
+		unsigned int max_move_index = 0;
+		
+		// Iterate thru all possible moves
+		for(unsigned int i = 0; i < possible_moves.size(); i++)
+		{
+			Board *temp_board = board.copy();
+			temp_board->doMove(minimax(possible_moves[i], msLeft, depth + 1), this->player_side);
+			
+			int score = temp_board->count(player_side) - temp_board->count(oppSide);
+			
+			int move_x = possible_moves[i]->x;
+			int move_y = possible_moves[i]->y;
+			
+			// Apply multipliers based on the cell location on the board
+			// and how favorable a move at that location would be.
+				 
+			// Corner squares are most preferable since these can never be captured
+			if((move_x == 0 && move_y ==0) || 
+			(move_x == 7 && move_y == 0) || 
+			(move_x == 0 && move_y == 7) || 
+			(move_x == 7 && move_y == 7))
+			{
+				score *= 5; 
+			}
+			// Squares adjacent to corners are worse - negative multiplier
+			// quadrant 1
+			else if((move_x == 0 && move_y == 1 )|| 
+			(move_x == 1 && (move_y == 0 || move_y == 1)))
+			{
+				score *= -3;
+			} 
+			// quadrant 2
+			else if((move_x == 6 && (move_y == 0 || move_y == 1)) || 
+			(move_x == 7 && move_y == 1))
+			{
+				score *= -3;
+			}
+			// quadrant 3
+			else if((move_x == 0 && move_y == 6 ) || 
+			(move_x == 1 && (move_y == 6 || move_y == 7)))
+			{
+				score *= -3;
+			}
+			// quadrant 4
+			else if((move_x == 6 && (move_y == 6 || move_y == 7) ) ||
+			 (move_x == 7 && move_y == 6))
+			{
+				score *= -3;
+			}
+				 
+			// Squares at the edge of the board can only be captured
+			// by other edge pieces - slightly favorable
+			else if((move_x == 0) || (move_x == 7) || (move_y == 0) || (move_y == 7))
+			{
+				score *= 3;
+			}
+			
+			// Set the highest score to this possible move
+			if(score > max_score)
+			{
+				max_move_index = i;
+				max_score = score;
+			}
+				 
+			// Clean up
+			delete temp_board;
+		}
+		
+		if (depth == 1) 
+		{
+            board.doMove(possible_moves[max_move_index], player_side);
+            return possible_moves[max_move_index];
+        }
+
+        else 
+        {
+            return possible_moves[max_move_index];
+        }	
 		
 	}
 	
-	for(int i = 0; i < possible_moves.size(); i++){
-		Board *temp_board = new Board();
-		temp_board.doMove(possible_moves[i], player_side);
-		//int score = temp_board->count(player_side) - temp_board->count(oppSide);
-		minimax(possible_moves[i], msLeft, depth + 1);
-		
-	}
+	return NULL;
 }
-*/
-
 
 Move *Player::doMove(Move *opponentsMove, int msLeft) 
 {
@@ -216,16 +259,15 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
 	 // Process opponent's move
      board.doMove(opponentsMove, oppSide);
      
-     if(testingMinimax){
-		 
-		 return minimax_iterative();
+     if(testingMinimax)
+     {
+		return minimax(opponentsMove, msLeft, 1);
 	 }
-	 else
-	 {
+     
      // Initialize variables
-		Move *highest_scoring = nullptr;
-		Move *player_move;
-		int highest_score = INT_MIN;
+	 Move *highest_scoring = nullptr;
+	 Move *player_move;
+	 int highest_score = INT_MIN;
 	 
 	 // Iterate through all board cells
 	 for(int i = 0; i < 8; i++)
@@ -244,7 +286,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
 				 // Assign a base score based on the difference between 
 				 // number of pieces on the board after the move has been made
 				 int score = temp_board->count(player_side) - temp_board->count(oppSide);
-				 
+
 				 // Apply multipliers based on the cell location on the board
 				 // and how favorable a move at that location would be.
 				 
@@ -281,6 +323,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
 				 {
 					 score *= 3;
 				 }
+				 
 
 				 // Set the highest score to this possible move
 				 if(score > highest_score)
@@ -299,5 +342,4 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
 	board.doMove(highest_scoring, player_side);
 	
     return highest_scoring;
-   }
 }
--- player.hpp
+++ player.hpp
@@ -16,6 +16,7 @@ public:
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
+    Move *minimax(Move *currentMove, int msLeft, int depth);
 	
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
